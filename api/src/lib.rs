2df8b255e1b0105c85f845959222a76957c5d2be
Lavion <ilavion4u@gmail.com>
2025-05-19 18:30:25 +0900

    fix: update dependencies & add library

---
+++ api/src/lib.rs
@@ -0,0 +1,11 @@
+// api/src/lib.rs
+// Entry point for the API library.
+
+pub mod routes;         // Routes
+pub mod configs;        // Configuration
+pub mod health;         // Health checks
+pub mod services;       // Business logic and services
+pub mod models;         // Data models
+pub mod repositories;   // Data repositories
+pub mod utilities;      // Utility functions and types
+pub mod tests;          // Unit Tests
