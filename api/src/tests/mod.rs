f580db3be312da654678dd1bdd5059e9b855d796
Lavion <ilavion4u@gmail.com>
2025-05-22 13:42:52 +0900

    feat: add tests module

---
+++ api/src/tests/mod.rs
@@ -0,0 +1,5 @@
+// src/tests/mod.rs
+
+mod validation_email;       // Email validation tests
+mod validation_password;    // Password validation tests
+mod validation_username;    // Username validation tests
